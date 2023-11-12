228f264283314740192ddf2f15d05e1c0c1aa137
Austincahall <austincahall.2000@gmail.com>
2023-11-12 11:57:31 -0500

    first attempt at homework 7

--- hw7/hw7-ddl.sql
+++ hw7/hw7-ddl.sql
@@ -18,6 +18,7 @@
 
 SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS skills
 # ... 
 SET FOREIGN_KEY_CHECKS=1;
 
@@ -28,11 +29,34 @@ SET FOREIGN_KEY_CHECKS=1;
 # time committment offers some sense of how much time was required (or will be required) to gain the skill.
 # You can assign the skill descriptions.  Please be creative!
 
+CREATE TABLE skills (
+    skills_id int not null,
+    skills_name varchar(255) not null,
+    skills_description varchar(255) not null,
+    skills_tag varchar(255) not null,
+    skills_url varchar(255),
+    skills_time_commitment int,
+    PRIMARY KEY (skills_id)
+);
+
+
+#select * from skills;
 
 # Section 3
 # Populate skills
 # Populates the skills table with eight skills, their tag fields must exactly contain “Skill 1”, “Skill 2”, etc.
 # You can assign skill names.  Please be creative!
+
+INSERT INTO skills (skills_id, skills_name, skills_description, skills_tag ) values
+(1,'kick boxing', 'skill to fight using hand and feet', 'skill 1'),
+(2,'coding', 'ability to problem solve in computer languages', 'skill 2'),
+(3,'Spanish speaking', 'ability to talk in spanish with anyone', 'skill 3'),
+(4,'singing', 'Being capable of making anyone cover their ears on the metro at ~ 2 in the morning', 'skill 4'),
+(5,'rock climbing', 'the ability to defy gravity and go where no man has gone, for good reason mostly', 'skill 5'),
+(6,'cooking', 'being capable of feeding oneself or many without the oven, because thats for baking', 'skill 6'),
+(7,'hunting', 'rumaging through the forest early at night and not finding anything', 'skill 7'),
+(8,'building', 'ability to use nature to construct un-naturally comfy pillows', 'skill 8')
+
 
 
 # Section 4
@@ -42,22 +66,46 @@ SET FOREIGN_KEY_CHECKS=1;
 
 CREATE TABLE people (
     people_id int,
+    people_first_name varchar(255),
     people_last_name varchar(256) NOT NULL,
+    people_email varchar(255),
+    people_linkedin_url varchar(255),
+    people_headshoturl varchar(255),
+    people_discord_handle varchar(255),
+    people_brief_bio varchar(255),
+    people_date_joined date,
     PRIMARY KEY (people_id)
 );
+#SELECT * FROM people;
 
 # Section 5
 # Populate people with six people.
 # Their last names must exactly be “Person 1”, “Person 2”, etc.
 # Other fields are for you to assign.
 
-insert into people (people_id,people_last_name) values (1,'Person 1');
+insert into people (people_id,people_first_name,people_discord_handle,people_date_joined, people_last_name) values 
+  (1, 'Ted', 'Mosby', '2005-09-19','Person 1'),
+  (2, 'Robin', 'Scherbatsky', '2006-09-19','Person 2'),
+  (3, 'Marshall', 'Eriksen', '2000-09-19','Person 3'),
+  (4, 'Lily', 'Aldrin', '2000-09-19','Person 4'),
+  (5, 'Barney', 'Stinson', '2005-09-19','Person 5'),
+  (6, 'Tracy', 'McConnell', '2013-05-27','Person 6');
 
+#SELECT * FROM people;
 
 # Section 6
 # Create peopleskills( id, skills_id, people_id, date_acquired )
 # None of the fields can ba NULL. ID can be auto_increment.
 
+CREATE TABLE peopleskills (
+    peopleskills_id INT AUTO_INCREMENT,
+    peopleskills_skills_id INT NOT NULL,
+    peopleskills_people_id INT NOT NULL,
+    peopleskills_date_acquired DATE NOT NULL,
+    PRIMARY KEY (peopleskills_id)
+);
+
+#SELECT * FROM peopleskills;
 
 # Section 7
 # Populate peopleskills such that:
